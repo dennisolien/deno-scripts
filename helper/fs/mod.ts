544e214c288acf8bd22b6fe3609ce2acb1c3d6d5
Dennis Lien <dennis.lien.o@gmail.com>
2020-06-03 16:54:46 +0200

    Add editFile - open a file in editor

--- helper/fs/mod.ts
+++ helper/fs/mod.ts
@@ -1,4 +1,5 @@
-import { ensureDir, emptyDir } from "https://deno.land/std@0.51.0/fs/mod.ts";
+import { ensureDir, ensureFile, emptyDir } from "https://deno.land/std@0.51.0/fs/mod.ts";
+import { run } from '../sub_process/mod.ts';
 
 export function getApplicationFolderPath(name:string) {
   const homeDir = Deno.env.get('HOME');
@@ -25,3 +26,20 @@ export function createApplicationSubFolder(applicationName: string, folderName: 
   const subFolderPath = getApplicationSubFolderPath(applicationName, folderName);
   return ensureDir(subFolderPath);
 }
+
+/**
+ * Open a filePath in a editor, vim by default.
+ * eg. await editFile('/Users/username/README.md', 'code');
+ */
+export async function editFile(filePath: string, openCommand?: string) {
+  await ensureFile(filePath);
+  // TODO: check os, and use os terminal editor.
+  const openWith = openCommand ? openCommand : 'vim';
+  const commands = [openWith, filePath];
+  const open = Deno.run({
+    cmd: commands,
+  });
+  
+  return open.status();
+}
+
